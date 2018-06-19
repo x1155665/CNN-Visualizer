dd465ab3f31b9eccb1fa2758ea6dc3b3511d8040
zir <zirconium299792458@gmail.com>
2018-06-19 03:42:59 +0200

    Add find_maxes from deepVis tool

--- find_maxes/misc.py
+++ find_maxes/misc.py
@@ -21,6 +21,8 @@ def load_network(settings):
 
     processed_prototxt = process_network_proto(settings.prototxt, settings.caffevis_caffe_root)
 
+    read_network_dag(settings, processed_prototxt)
+
     net = caffe.Classifier(processed_prototxt, settings.network_weights, mean=settings.mean, raw_scale=255.0,
                            channel_swap=settings.channel_swap)
 
@@ -51,6 +53,120 @@ def process_network_proto(prototxt, caffevis_caffe_root):
     os.system(upgrade_tool_command_line)
 
     return processed_prototxt
+
+
+def read_network_dag(settings, processed_prototxt):
+    from caffe.proto import caffe_pb2
+    from google.protobuf import text_format
+
+    # load prototxt file
+    network_def = caffe_pb2.NetParameter()
+    with open(processed_prototxt, 'r') as proto_file:
+        text_format.Merge(str(proto_file.read()), network_def)
+
+    # map layer name to layer record
+    layer_name_to_record = dict()
+    for layer_def in network_def.layer:
+        if (len(layer_def.include) == 0) or (caffe_pb2.TEST in [item.phase for item in layer_def.include]):
+            layer_name_to_record[layer_def.name] = LayerRecord(layer_def)
+
+    top_to_layers = dict()
+    for layer in network_def.layer:
+        # no specific phase, or TEST phase is specifically asked for
+        if (len(layer.include) == 0) or (caffe_pb2.TEST in [item.phase for item in layer.include]):
+            for top in layer.top:
+                if top not in top_to_layers:
+                    top_to_layers[top] = list()
+                top_to_layers[top].append(layer.name)
+
+    # find parents and children of all layers
+    for child_layer_name in layer_name_to_record.keys():
+        child_layer_def = layer_name_to_record[child_layer_name]
+        for bottom in child_layer_def.bottoms:
+            for parent_layer_name in top_to_layers[bottom]:
+                if parent_layer_name in layer_name_to_record:
+                    parent_layer_def = layer_name_to_record[parent_layer_name]
+                    if parent_layer_def not in child_layer_def.parents:
+                        child_layer_def.parents.append(parent_layer_def)
+                    if child_layer_def not in parent_layer_def.children:
+                        parent_layer_def.children.append(child_layer_def)
+
+    # update filter, strid, pad for maxout "structures"
+    for layer_name in layer_name_to_record.keys():
+        layer_def = layer_name_to_record[layer_name]
+        if layer_def.type == 'Eltwise' and \
+           len(layer_def.parents) == 1 and \
+           layer_def.parents[0].type == 'Slice' and \
+           len(layer_def.parents[0].parents) == 1 and \
+           layer_def.parents[0].parents[0].type in ['Convolution', 'InnerProduct']:
+            layer_def.filter = layer_def.parents[0].parents[0].filter
+            layer_def.stride = layer_def.parents[0].parents[0].stride
+            layer_def.pad = layer_def.parents[0].parents[0].pad
+
+    # keep helper variables in settings
+    settings._network_def = network_def
+    settings._layer_name_to_record = layer_name_to_record
+
+    return
+
+
+class LayerRecord:
+
+    def __init__(self, layer_def):
+
+        self.layer_def = layer_def
+        self.name = layer_def.name
+        self.type = layer_def.type
+
+        # keep filter, stride and pad
+        if layer_def.type == 'Convolution':
+            self.filter = list(layer_def.convolution_param.kernel_size)
+            if len(self.filter) == 1:
+                self.filter *= 2
+            self.pad = list(layer_def.convolution_param.pad)
+            if len(self.pad) == 0:
+                self.pad = [0, 0]
+            elif len(self.pad) == 1:
+                self.pad *= 2
+            self.stride = list(layer_def.convolution_param.stride)
+            if len(self.stride) == 0:
+                self.stride = [1, 1]
+            elif len(self.stride) == 1:
+                self.stride *= 2
+
+        elif layer_def.type == 'Pooling':
+            self.filter = [layer_def.pooling_param.kernel_size]
+            if len(self.filter) == 1:
+                self.filter *= 2
+            self.pad = [layer_def.pooling_param.pad]
+            if len(self.pad) == 0:
+                self.pad = [0, 0]
+            elif len(self.pad) == 1:
+                self.pad *= 2
+            self.stride = [layer_def.pooling_param.stride]
+            if len(self.stride) == 0:
+                self.stride = [1, 1]
+            elif len(self.stride) == 1:
+                self.stride *= 2
+
+        else:
+            self.filter = [0, 0]
+            self.pad = [0, 0]
+            self.stride = [1, 1]
+
+        # keep tops
+        self.tops = list(layer_def.top)
+
+        # keep bottoms
+        self.bottoms = list(layer_def.bottom)
+
+        # list of parent layers
+        self.parents = []
+
+        # list of child layers
+        self.children = []
+
+    pass
 
 
 def mkdir_p(path):
